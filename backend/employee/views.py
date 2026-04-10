9a9e6ee9297cce90d4649d45b74ef71bf5d64a15
SIVANESH4 <sivanesh6363@gmail.com>
2026-04-10 20:17:44 +0530

    Learnt Viewsets

--- backend/employee/views.py
+++ backend/employee/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.views import APIView
 from .serializers import EmployeeSerializer
 from .models import Employee_table
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, viewsets
 
 # Create your views here.
 '''
@@ -26,8 +26,10 @@ class EmployeesDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixin
     def put(self, request, pk):
         return self.update(request, pk)
     def delete(self, request, pk):
-        return self.destroy(request, pk)'''
+        return self.destroy(request, pk)
+'''
 
+'''
 class Employees(generics.ListCreateAPIView):
     queryset = Employee_table.objects.all()
     serializer_class = EmployeeSerializer
@@ -36,3 +38,19 @@ class EmployeesDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee_table.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'pk'
+'''
+
+'''class EmployeeViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Employee_table.objects.all()
+        serializer = EmployeeSerializer(queryset, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def retrieve(self, request, pk=None):
+        employee = get_object_or_404(Employee_table,pk=pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data)'''
+        
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee_table.objects.all()
+    serializer_class = EmployeeSerializer
